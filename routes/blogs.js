7365faf985b3fdded3933671eb4e7256150bc9d3
Kevin <kevdougful@gmail.com>
2015-09-14 15:55:59 -0500

    added new routes

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,12 +2,43 @@ var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
 
-/* Create a new blog, then respond with new id */
+/* GET all blogs from DB */
+router.get('/', function(req, res) {
+	models.Blog.findAll({
+		include: [ 
+			models.Post 
+		]
+	}).catch(function(error) {
+		res.json(error.message);
+	}).then(function(blogs) {
+		res.json(blogs);
+	});
+});
+
+/* GET a blog from DB by id */
+router.get('/:blog_id', function(req, res) {
+	models.Blog.findOne({
+		include: [ 
+			models.Post
+		],
+		where: {
+			id: req.params.blog_id
+		}
+	}).catch(function(error) {
+		res.json(error.message);
+	}).then(function(blog) {
+		res.json(blog);
+	});
+});
+
+/* Create a new blog, then respond with new blog */
 router.post('/create', function(req, res) {
 	models.Blog.create({
 		Name: req.body.name,
 		Description: req.body.description,
 		DateCreated: Date()
+	}).catch(function(error) {
+		res.json(error.message);
 	}).then(function(newBlog) {
 		res.json({ 
 			id: newBlog.id,
@@ -15,6 +46,47 @@ router.post('/create', function(req, res) {
 			Description: newBlog.Description,
 			DateCreated: newBlog.DateCreated
 		});
+	});
+});
+
+/* Update a certain existing blog record */
+router.put('/:blog_id/update', function(req, res) {
+	// Only allow certain fields to be updated
+	var newValues = {};
+	if (req.body.Name !== null)
+		newValues.Name = req.body.Name;
+	if (req.body.Description !== null)
+		newValues.Description = req.body.Description;
+
+	// Update record
+	models.Blog.update(
+		newValues,
+		{
+			where: { id: req.params.blog_id }
+		}
+	).then(function(effectedRows) {
+		res.json({
+			effectedRows: effectedRows
+		});
+	}).catch(function() {
+		res.json(error.message);
+	});
+});
+
+/* Create a new post on a specific blog */
+router.post('/:blog_id/posts/create', function(req, res) {
+	models.Post.create({
+		Title: req.body.title,
+		Content: req.body.content,
+		DateCreated: Date(),
+		CommentsAllowed: req.body.commentsAllowed,
+		BlogId: req.params.blog_id,
+		TopicId: 1,
+		BlogUserId: 1
+	}).catch(function(error) {
+		res.json(error.message);
+	}).then(function(newPost) {
+		res.json(newPost);
 	});
 });
 
