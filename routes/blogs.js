dbf06409d8955e3291e7b1ac47826881ca0e69dd
Kevin <kevdougful@gmail.com>
2015-09-14 14:02:04 -0500

    added .then to blogs/create route

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,9 +2,19 @@ var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
 
+/* Create a new blog, then respond with new id */
 router.post('/create', function(req, res) {
 	models.Blog.create({
-		Name: req.body.name
+		Name: req.body.name,
+		Description: req.body.description,
+		DateCreated: Date()
+	}).then(function(newBlog) {
+		res.json({ 
+			id: newBlog.id,
+			Name: newBlog.Name,
+			Description: newBlog.Description,
+			DateCreated: newBlog.DateCreated
+		});
 	});
 });
 
