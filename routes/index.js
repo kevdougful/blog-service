bcaf158ed8161d2d5248e358b9b4ebbf83944d3a
Kevin Coffey <kevdougful@gmail.com>
2015-09-17 00:49:57 -0500

    creates token

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,53 @@
 /* jshint node:true */
 'use strict';
 
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Authenticate */
+router.post('/auth', function(req, res) {
+    models.BlogUser.findOne({
+        where: {
+            AuthUserStringId: req.body.username    
+        }
+    }).catch(function(error) {
+        res.json({
+            'success': false,
+            'error': 'Authentication failed.' + error.message
+        });
+    }).then(function(user) {
+        
+        // User not found
+        if (!user) {
+            res.json({
+                'success': false,
+                'error': 'Authentication failed. User not found.'
+            });
+        }
+        
+        // User found, wrong password
+        if (user.Password != req.body.password) {
+            res.json({
+                'success': false,
+                'error': 'Authentication failed. Wrong password.'
+            });
+        } else {
+            // User found, password matches
+            // Create a token
+            var secret = require('../config/secret').secret;
+            var token = jwt.sign(user, secret, {
+                expiresInMinutes: 1440 // 24 hours
+            });
+            
+            // Return new token
+            res.json({
+                'success': true,
+                'token': token
+            });
+        }
+    });
 });
 
 module.exports = router;
