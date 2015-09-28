2d0275081994b19e71dd66277bef08aa04a3c4ff
Chris <chris@pixelmixr.com>
2015-09-27 22:58:38 -0400

    Adding gulpfile.js.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var mocha = require('gulp-mocha');
+
+gulp.task('default', function() {
+	nodemon({
+		script: 'app.js',
+		ext: 'js',
+		env: {
+			PORT: 3000
+		},
+		ignore: ['./node_modules/**']
+	}).on('restart', function() {
+		console.log('restarting');
+	});
+});
+
+gulp.task('test', function() {
+	gulp.src('tests/*.js', { read: false })
+		.pipe(mocha({ reporter: 'nyan' }));
+})
